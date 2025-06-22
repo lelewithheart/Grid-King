00041244c82c730555fba360e08b780c5f29c8a7
lelewithheart <115990008+lelewithheart@users.noreply.github.com>
2025-06-22 21:21:04 +0200

    1.1.0a - Bugfixes
    The logos href to index.php is no longer a relative path, so uding it on any /admin/ page won't break the website anymore

--- includes/header.php
+++ includes/header.php
@@ -94,7 +94,7 @@ $themeColor = $settings['theme_color'] ?? '#dc2626';
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="index.php">
+            <a class="navbar-brand" href="/index.php">
                 <i class="bi bi-flag-checkered me-2"></i><?php echo APP_NAME; ?>
             </a>
             
